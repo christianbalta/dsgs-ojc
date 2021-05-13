298318bb0657f2d0501a9f704a3439f18eb80ccb
christianbalta <christianbalta@outlook.com>
2021-05-13 16:04:48 +0200

    Connect to DB

--- src/components/Alphabet.js
+++ src/components/Alphabet.js
@@ -1,16 +1,47 @@
-import React from 'react';
-import {ListGroup, Row} from "react-bootstrap";
+import React, {useEffect, useState} from 'react';
+import {Image, ListGroup} from "react-bootstrap";
+import axios from "axios";
 
 const Alphabet = () => {
+    const [letters, setLetters] = useState([]);
+    const [image, setImage] = useState('https://firebasestorage.googleapis.com/v0/b/dsgs-ojc.appspot.com/o/A.jpg?alt=media&token=806df21d-4a6c-428d-abd7-7039731d160c');
+
+    useEffect(function () {
+        async function getLetters() {
+            try {
+                const response = await axios.get("/api/letters");
+                setLetters(response.data);
+            } catch (error) {
+                console.log('error', error);
+            }
+        }
+
+        getLetters();
+    }, []);
+
+    const getImage = (imageUrl) => {
+        setImage(imageUrl)
+    }
+
     return (
-        <ListGroup horizontal>
-            <ListGroup.Item>A</ListGroup.Item>
-            <ListGroup.Item>B</ListGroup.Item>
-            <ListGroup.Item>C</ListGroup.Item>
-            <ListGroup.Item>D</ListGroup.Item>
-            <ListGroup.Item>E</ListGroup.Item>
-            <ListGroup.Item>...</ListGroup.Item>
-        </ListGroup>
+        <>
+            <ListGroup horizontal>
+                {
+                    letters.map((letter) => {
+                        return (
+                            <div key={letter._id}>
+                                <ListGroup.Item
+                                    onClick={() => getImage(letter.imageUrl)}
+                                    className="letter">{letter.letter}</ListGroup.Item>
+                            </div>
+                        )
+                    })
+                }
+            </ListGroup>
+            <div className="text-center">
+                <Image src={image} fluid className="mt-5 image"/>
+            </div>
+        </>
     );
 };
 
--- src/components/Sentence.js
+++ src/components/Sentence.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {ListGroup} from "react-bootstrap";
+import axios from "axios";
 
 const Sentence = () => {
+    const [sentences, setSentences] = useState([]);
+    const [video, setVideo] = useState('https://firebasestorage.googleapis.com/v0/b/dsgs-ojc.appspot.com/o/hallo2.mp4?alt=media&token=ef058362-2a1f-4ad4-8437-1935dc78f1b6');
+
+    useEffect(function () {
+        async function getSentence() {
+            try {
+                const response = await axios.get("/api/sentences");
+                setSentences(response.data);
+            } catch (error) {
+                console.log('error', error);
+            }
+        }
+
+        getSentence();
+    }, []);
+
+    const getVideo = (videoUrl) => {
+        setVideo(videoUrl)
+    }
+
     return (
-        <ListGroup horizontal>
-            <ListGroup.Item>Hallo</ListGroup.Item>
-            <ListGroup.Item>Wie gehts?</ListGroup.Item>
-            <ListGroup.Item>Mein Name ist</ListGroup.Item>
-            <ListGroup.Item>Dónde está la biblioteca?</ListGroup.Item>
-            <ListGroup.Item>...</ListGroup.Item>
-        </ListGroup>
+        <>
+            <ListGroup horizontal>
+                {
+                    sentences.map((sentence) => {
+                        return (
+                            <div key={sentence._id}>
+                                <ListGroup.Item
+                                    onClick={() => getVideo(sentence.videoUrl)}
+                                    className="letter">{sentence.sentence}</ListGroup.Item>
+                            </div>
+                        )
+                    })
+                }
+            </ListGroup>
+            <div className="text-center mt-5 mb-5">
+                <video src={video} controls className="video">
+                    no video
+                </video>
+            </div>
+        </>
     );
 };
 
